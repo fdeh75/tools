c81435233ef1d9b259a9f3c55892a798e946da73
fdeh75 <fdeh75@mail.ru>
2020-11-29 04:24:12 +0300

    Update start_install.sh

--- start_install.sh
+++ start_install.sh
@@ -7,6 +7,7 @@ tic -x termite.terminfo
 apt-get update
 apt-get upgrade
 apt-get install zsh
+apt-get install git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 

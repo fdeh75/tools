e291c926e97bcaf07b8aa51b96cd2cd426e52a0b
fdeh75 <fdeh75@mail.ru>
2021-01-02 09:27:55 +0300

    Update start_install.sh
    Add no confirm

--- start_install.sh
+++ start_install.sh
@@ -5,26 +5,24 @@ wget https://raw.githubusercontent.com/thestinger/termite/master/termite.terminf
 tic -x termite.terminfo
 
 apt-get update
-apt-get upgrade
-apt-get install \
+apt-get -y upgrade
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-apt-get install curl
+apt-get -y install curl
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-apt-get install zsh
-apt-get install git
+apt-get -y install zsh
+apt-get -y install git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-

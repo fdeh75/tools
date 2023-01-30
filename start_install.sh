d00a61c9ae75f5c82adc4fe07d613bad1e8d6e8e
fdeh75 <fdeh75@mail.ru>
2023-01-30 15:06:48 +0300

    Update start_install.sh

--- start_install.sh
+++ start_install.sh
@@ -1,28 +1,16 @@
 #!/usr/bin bash
 
-cd  /tmp
-wget https://raw.githubusercontent.com/thestinger/termite/master/termite.terminfo
-tic -x termite.terminfo
-
 apt-get update
 apt-get -y upgrade
 apt-get -y install \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
-apt-get -y install curl
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+    gnupg \
+    lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-apt-get -y install zsh
-apt-get -y install git
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin

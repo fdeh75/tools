aeaa2880861857913c057b78a73dc8ee436e517c
fdeh <fdeh75@gmail.com>
2024-01-21 00:18:25 +0300

    Use docker_install.sh separate
    Update start_install.sh

--- start_install.sh
+++ start_install.sh
@@ -1,30 +1,9 @@
 #!/usr/bin bash
 
-cd  /tmp
-wget https://raw.githubusercontent.com/thestinger/termite/master/termite.terminfo
-tic -x termite.terminfo
+apt install sudo || true
 
-apt-get update
-apt-get upgrade
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-apt-get install curl
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-apt-get install zsh
-apt-get install git
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt update
+sudo apt update
+sudo apt install vim
 
-
+. docker_install.sh
